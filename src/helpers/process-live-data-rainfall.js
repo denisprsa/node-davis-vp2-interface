a3f862dd31df6d29bfd2a8eee91ece334d9b153d
denisp <denis.prsa@inova.si>
2018-07-15 15:48:51 +0200

    Added import

--- src/helpers/process-live-data-rainfall.js
+++ src/helpers/process-live-data-rainfall.js
@@ -1,29 +1,37 @@
 const fs = require('fs');
 
 module.exports = (currentDate, currentRainfall, config) => {
+    currentDate.setMinutes(currentDate.getMinutes() - 15); 
     let data = fs.readFileSync(config.fileDBLocation, 'utf-8');
-    var lines = data.trim().split('\n');
-    
-    var lastLines = lines.slice(-20);
+    let lines = data.trim().split('\n');
+    let sumRain = 0;
+    let lastLines = lines.slice(-20);
     
     console.log(lastLines);
-    /*
-    Logger.log('Last Time: ', lastLine);
 
-    let arrayOfDataInLine = lastLine.split(',');
-    let dateTimeArrayLine = arrayOfDataInLine[0].split(' ');
-    let dateArrayLine = dateTimeArrayLine[0].split('.');
-    let timeArrayLine = dateTimeArrayLine[1].split(':');
+    for (line of lastLines) {
+        console.log('Archive Line: ', line);
 
-    let lastDate = new Date();
-    lastDate.setFullYear(parseInt(dateArrayLine[2]));
-    lastDate.setMonth(parseInt(dateArrayLine[1] - 1));
-    lastDate.setDate(dateArrayLine[0]);
-    lastDate.setHours(timeArrayLine[0]);
-    lastDate.setMinutes(timeArrayLine[1]);
-    lastDate.setSeconds(0);
-    lastDate.setMilliseconds(0);
+        let arrayOfDataInLine = line.split(',');
+        let dateTimeArrayLine = arrayOfDataInLine[0].split(' ');
+        let dateArrayLine = dateTimeArrayLine[0].split('.');
+        let timeArrayLine = dateTimeArrayLine[1].split(':');
+    
+        let lastDate = new Date();
+        lastDate.setFullYear(parseInt(dateArrayLine[2]));
+        lastDate.setMonth(parseInt(dateArrayLine[1] - 1));
+        lastDate.setDate(dateArrayLine[0]);
+        lastDate.setHours(timeArrayLine[0]);
+        lastDate.setMinutes(timeArrayLine[1]);
+        lastDate.setSeconds(0);
+        lastDate.setMilliseconds(0);
 
-    return lastDate;
-    */
+        if (currentDate <= lastDate) {
+            rain = arrayOfDataInLine[arrayOfDataInLine.length - 1]
+            console.log(rain)
+            sumRain += Number(rain);
+        }
+    }
+
+    return sumRain - currentRainfall;
 };
