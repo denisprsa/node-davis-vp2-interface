065d151e1081c669ac6b6047be09fb0fca8a8ec2
denisp <denis.prsa@inova.si>
2020-08-12 12:38:55 +0200

    Removed console.log

--- src/helpers/process-live-data-rainfall.js
+++ src/helpers/process-live-data-rainfall.js
@@ -3,20 +3,12 @@ module.exports = (currentDate, currentRainfall, data = []) => {
     currentDate.setMinutes(currentDate.getMinutes() - 60);
     let sumRain = 0;
 
-    console.log('process live data rainfall');
-    console.log(data);
-
     for (let line of data) {
         if (currentDate <= line.date) {
             let rain = line.rainfall;
             sumRain += Number(rain);
         }
     }
-
-    console.log(sumRain);
-    console.log(currentRainfall);
-    console.log(currentRainfall - sumRain);
-    console.log('====  end ====');
 
     return currentRainfall - sumRain;
 };
